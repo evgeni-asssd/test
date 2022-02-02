cc28ed6c597ffd928888a95f45f188d9a72ceca1
evgeni <kluev.evga@gmail.com>
2022-02-02 17:17:20 +0300

    q clear

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+
+const date = new Date("02, 10, 2022");
+
+console.log(date.toString());
